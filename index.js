ed9a6166d43f728335d34715be1c7b4abe2b0820
Danny Chen <dannychen@Dannys-Air.lan1>
2024-07-02 22:48:18 -0400

    feat: arrow keys to move note: collision detection not yet implemented

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 // FOR LATER USE
-// window.addEventListener("keydown", function (e) {
-//     let box = document.querySelector(".box");
-//     if (e.key == "ArrowDown") {
-//         console.log("down arrow clicked")
-//         box.style.top = `${box.offsetTop + box.offsetHeight}px`
-//     }
-// });
+function addArrowCtrls() {
+    window.addEventListener("keydown", function (e) {
+        let testCell = document.querySelector(".test-cell");
+        if (e.key == "ArrowDown") {
+            console.log("down arrow clicked");
+            // console.log(`${testCell.offsetLeft}, ${testCell.offsetTop}`);
+            testCell.style.top = `${testCell.offsetTop + testCell.offsetHeight}px`;
+            // console.log(`${testCell.offsetLeft}, ${testCell.offsetTop}`);
+        }
+
+        if (e.key == "ArrowUp") {
+            console.log("up arrow clicked");
+            testCell.style.top = `${testCell.offsetTop - testCell.offsetHeight}px`;
+        }
+
+        if (e.key == "ArrowLeft") {
+            console.log("left arrow clicked");
+            testCell.style.left = `${testCell.offsetLeft - testCell.offsetWidth}px`;
+        }
+
+        if (e.key == "ArrowRight") {
+            console.log("right arrow clicked");
+            testCell.style.left = `${testCell.offsetLeft + testCell.offsetWidth}px`;
+        }
+    }); 
+}
+
 
 function createEleWithCls(ele, clsArr) {
     let element = document.createElement(ele);
@@ -29,13 +49,18 @@ function createBoard() {
     }
 }
 
-function createBlocks() {
-
-}
-
 
 function main() {
     createBoard();
+    addArrowCtrls()
 }
 
 main();
+
+// Test functions
+function getCoor(cellNum) {
+    let cell = document.querySelector(cellNum);
+    console.log(cell.offsetLeft);
+}
+
+getCoor(".cell-2")
