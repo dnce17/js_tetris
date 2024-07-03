f0f3172887088f11f522f1dcb2b89c129d178f48
Danny Chen <dannychen@Dannys-Air.lan1>
2024-07-03 15:15:38 -0400

    feat: collision detection added

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Credits
 * https://jsfiddle.net/robertc/qVP8r/ 
 * https://stackoverflow.com/questions/13691699/styling-every-3rd-item-of-a-list-using-css
+* https://stackoverflow.com/questions/12273451/how-to-fix-delay-in-javascript-keydown
--- index.js
+++ index.js
@@ -1,32 +1,4 @@
-// FOR LATER USE
-function addArrowCtrls() {
-    window.addEventListener("keydown", function (e) {
-        let testCell = document.querySelector(".test-cell");
-        if (e.key == "ArrowDown") {
-            console.log("down arrow clicked");
-            // console.log(`${testCell.offsetLeft}, ${testCell.offsetTop}`);
-            testCell.style.top = `${testCell.offsetTop + testCell.offsetHeight}px`;
-            // console.log(`${testCell.offsetLeft}, ${testCell.offsetTop}`);
-        }
-
-        if (e.key == "ArrowUp") {
-            console.log("up arrow clicked");
-            testCell.style.top = `${testCell.offsetTop - testCell.offsetHeight}px`;
-        }
-
-        if (e.key == "ArrowLeft") {
-            console.log("left arrow clicked");
-            testCell.style.left = `${testCell.offsetLeft - testCell.offsetWidth}px`;
-        }
-
-        if (e.key == "ArrowRight") {
-            console.log("right arrow clicked");
-            testCell.style.left = `${testCell.offsetLeft + testCell.offsetWidth}px`;
-        }
-    }); 
-}
-
-
+// REUSABLE
 function createEleWithCls(ele, clsArr) {
     let element = document.createElement(ele);
 
@@ -34,8 +6,62 @@ function createEleWithCls(ele, clsArr) {
         element.classList.add(item);
     }
 
-    return element
+    return element;
 }
+// ---
+
+// IN PROGRESS: must implement collision
+let testCell = document.querySelector(".test-cell");
+let blockWidth = 24;
+let blockHeight = 24;
+let keyState = {
+    "ArrowDown": null,
+    "ArrowLeft": null,
+    "ArrowRight": null
+};
+let boundaries = {
+    "bottom": 480,
+    "left": 0,
+    "right": 216 - blockWidth
+}
+
+function addArrowCtrls() {
+    window.addEventListener("keydown", function (e) {
+        if (e.key == "ArrowDown" || e.key == "ArrowLeft" || e.key == "ArrowRight" || e.key == "ArrowUp") {
+            keyState[e.key] = true;
+            // console.log(`keydown: ${keyState[e.key]}`);
+        }
+    }); 
+
+    window.addEventListener("keyup", function(e) {
+        if (e.key == "ArrowDown" || e.key == "ArrowLeft" || e.key == "ArrowRight" || e.key == "ArrowUp") {
+            keyState[e.key] = false;
+            // console.log(`keyup: ${keyState[e.key]}`);
+        }
+    })
+}
+
+function gameLoop(e) {
+    if (keyState["ArrowDown"] == true && testCell.offsetTop < boundaries["bottom"]) {
+        testCell.style.top = `${testCell.offsetTop + blockHeight}px`;
+    }    
+    if (keyState["ArrowLeft"] == true && testCell.offsetLeft > boundaries["left"]) {
+        testCell.style.left = `${testCell.offsetLeft - blockWidth}px`;
+    }
+    if (keyState["ArrowRight"] == true && testCell.offsetLeft < boundaries["right"]) {
+        testCell.style.left = `${testCell.offsetLeft + blockWidth}px`;
+    }
+
+    // TEST USE ONLY
+    if (keyState["ArrowUp"] == true) {
+        testCell.style.top = `${testCell.offsetTop - blockHeight}px`;
+    }
+
+    // console.log(`${testCell.offsetLeft}, ${testCell.offsetTop}`);
+
+    // Controls speed that block moves
+    setTimeout(gameLoop, 30);
+} 
 
 function createBoard() {
     let board = document.querySelector(".board");
@@ -49,18 +75,44 @@ function createBoard() {
     }
 }
 
+// IN PROGRESS
+function blockTypes() {
+    let blocks = {
+        "O": [
+            [1, 1],
+            [1, 1]
+        ],
+        "I": [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [1, 1, 1, 1]
+        ]
+    }
+
+    return blocks
+}
+
 
 function main() {
     createBoard();
-    addArrowCtrls()
+    addArrowCtrls();
+    gameLoop();
 }
 
 main();
 
 // Test functions
 function getCoor(cellNum) {
-    let cell = document.querySelector(cellNum);
-    console.log(cell.offsetLeft);
+    let cell = document.querySelector("." + cellNum);
+    let cellCoor = {
+        "offsetLeft": cell.offsetLeft,
+        "offsetTop": cell.offsetTop
+    }
+    // console.log(cell.offsetLeft);
+    // console.log(`Cell Coor: ${cell.offsetLeft}, ${cell.offsetTop}`)
+
+    return cellCoor;
 }
 
-getCoor(".cell-2")
+// getCoor("cell-1")
