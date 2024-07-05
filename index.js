e808d7e82b392af0101fcad0652df44f0f0d9b28
Danny Chen <dannychen@Dannys-Air.lan1>
2024-07-05 11:40:34 -0400

    feat: block collision

--- index.js
+++ index.js
@@ -8,12 +8,20 @@ function createEleWithCls(ele, clsArr) {
 
     return element;
 }
-// ---
 
-// IN PROGRESS: must implement collision
-let testCell = document.querySelector(".test-cell");
-let blockWidth = 24;
-let blockHeight = 24;
+// TETRIS BLOCK INFO
+const PX_WIDTH = 24;
+const PX_HEIGHT = 24;
+const COL_IDENTIFIERS = {
+    1: [0, 3, 6],
+    2: [1, 4, 7],
+    3: [2, 5, 8],
+};
+const ROW_IDENTIFIERS = {
+    1: [0, 1, 2],
+    2: [3, 4, 5],
+    3: [6, 7, 8],
+};
 let keyState = {
     "ArrowDown": null,
     "ArrowLeft": null,
@@ -22,8 +30,10 @@ let keyState = {
 let collision = {
     "bottom": 480,
     "left": 0,
-    "right": 216 - blockWidth
-}
+    "right": 216 - (PX_WIDTH * 2)
+};
+
+let testCell = document.querySelector(".test-cell");
 
 function addArrowCtrls() {
     window.addEventListener("keydown", function (e) {
@@ -43,91 +53,64 @@ function addArrowCtrls() {
 
 function gameLoop(e) {
     if (keyState["ArrowDown"] == true && testCell.offsetTop < collision["bottom"]) {
-        testCell.style.top = `${testCell.offsetTop + blockHeight}px`;
+        testCell.style.top = `${testCell.offsetTop + PX_HEIGHT}px`;
     }    
     if (keyState["ArrowLeft"] == true && testCell.offsetLeft > collision["left"]) {
-        testCell.style.left = `${testCell.offsetLeft - blockWidth}px`;
+        testCell.style.left = `${testCell.offsetLeft - PX_WIDTH}px`;
     }
     if (keyState["ArrowRight"] == true && testCell.offsetLeft < collision["right"]) {
-        testCell.style.left = `${testCell.offsetLeft + blockWidth}px`;
+        testCell.style.left = `${testCell.offsetLeft + PX_WIDTH}px`;
     }
 
     // TEST USE ONLY
     if (keyState["ArrowUp"] == true) {
-        testCell.style.top = `${testCell.offsetTop - blockHeight}px`;
+        testCell.style.top = `${testCell.offsetTop - PX_HEIGHT}px`;
     }
-
-    // console.log(`${testCell.offsetLeft}, ${testCell.offsetTop}`);
 
     // Controls speed that block moves
     setTimeout(gameLoop, 30);
 } 
 
-function detectBlockBoundaries() {
-    // Check which columns has a block piece
-    let cols = {1: false, 2: false, 3: false}
-    let colIdentifers = {
-        1: [0, 3, 6],
-        2: [1, 4, 7],
-        3: [2, 5, 8],
+// NOTE: Next 3 funcs below relate to block collision detection
+function collisionInfo(direction, axisVal) {
+    // Collision values based on block's rotation (aka which col + row is occupied in 3x3 grid of block)
+    switch (direction) {
+        case "right":
+            return axisVal == 3 ? 216 - PX_WIDTH * 2 : 216 - PX_WIDTH;
+        case "left":
+            return axisVal == 1 ? 0 : 0 - PX_WIDTH;
+        case "bottom":
+            return axisVal == 3 ? 480 : 480 + PX_HEIGHT;
     }
-    // Check which row has a block piece
-    let rows = {1: false, 2: false, 3: false}
-    let rowIdentifers = {
-        1: [0, 1, 2],
-        2: [3, 4, 5],
-        3: [6, 7, 8],
-    }
+}
 
+function processCollision(block, axis, axisIdentifer, axisValArr, direction) {
+    let valA = axisValArr[0];
+    let valB = axisValArr[1];
 
-    for (let i = 0; i < testCell.children.length; i++) {
-        // Check right boundary
-        if (testCell.children[i].classList.contains("filled")) {
-            if (colIdentifers["3"].includes(i)) {
-                cols["3"] = true;
-                collision["right"] = 216 - blockWidth * 2;
-                console.log("column 3 has item")
+    for (let i = 0; i < block.children.length; i++) {
+        if (block.children[i].classList.contains("filled")) {
+            if (axisIdentifer[valA].includes(i)) {
+                axis[valA] = true;
+                collision[direction] = collisionInfo(direction, valA);
             }
-            else if (colIdentifers["2"].includes(i) && cols["3"] == false) {
-                cols["2"] = true;
-                collision["right"] = 216 - blockWidth;
-                console.log("column 2 has item")
-            }
-        }
-
-        // Check left boundary
-        if (testCell.children[i].classList.contains("filled")) {
-            // This may be redundant sinc default is left 0, but you will need to change
-            // cols["1"] to true as default
-            if (colIdentifers["1"].includes(i)) {
-                cols["1"] = true;
-                collision["left"] = 0;
-                console.log("column 1 has item");
-            }
-            else if (colIdentifers["2"].includes(i) && cols["1"] == false) {
-                cols["2"] = true;
-                collision["left"] = 0 - blockWidth;
-                console.log("column 2 has item");
-            }
-        }
-
-        console.log(cols);
-
-        if (testCell.children[i].classList.contains("filled")) {
-            // Check bottom boundary
-            if (rowIdentifers["3"].includes(i)) {
-                rows["3"] == true;
-                collision["bottom"] = 480;
-            }
-            else if (rowIdentifers["2"].includes(i) && rows["3"] == false) {
-                rows["2"] == true;
-                collision["bottom"] = 480 + blockHeight;
+            else if (axisIdentifer[valB].includes(i) && axis[valA] == false) {
+                axis[valB] = true;
+                collision[direction] = collisionInfo(direction, valB);
             }
         }
     }
 }
 
-detectBlockBoundaries();
+function detectCollision() {
+    // Check which cols + rows is occupied in block's 3x3 grid
+    let cols = {1: false, 2: false, 3: false}
+    let rows = {1: false, 2: false, 3: false}
+
+    processCollision(testCell, cols, COL_IDENTIFIERS, [3, 2], "right");
+    processCollision(testCell, cols, COL_IDENTIFIERS, [1, 2], "left");
+    processCollision(testCell, rows, ROW_IDENTIFIERS, [3, 2], "bottom");
+}
 
 
 function createBoard() {
@@ -142,44 +125,11 @@ function createBoard() {
     }
 }
 
-// IN PROGRESS
-function blockTypes() {
-    let blocks = {
-        "O": [
-            [1, 1],
-            [1, 1]
-        ],
-        "I": [
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-            [1, 1, 1, 1]
-        ]
-    }
-
-    return blocks
-}
-
-
 function main() {
     createBoard();
     addArrowCtrls();
     gameLoop();
+    detectCollision();
 }
 
 main();
-
-// Test functions
-function getCoor(cellNum) {
-    let cell = document.querySelector("." + cellNum);
-    let cellCoor = {
-        "offsetLeft": cell.offsetLeft,
-        "offsetTop": cell.offsetTop
-    }
-    // console.log(cell.offsetLeft);
-    // console.log(`Cell Coor: ${cell.offsetLeft}, ${cell.offsetTop}`)
-
-    return cellCoor;
-}
-
-// getCoor("cell-1")
